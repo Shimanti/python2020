0e309b3e75e76f2b8571bfbbac9119fddeea3d01
Shimanti <676501+Shimanti@users.noreply.github.com>
2021-07-08 13:31:02 -0700

    removed key

--- cookbook/settings.py
+++ cookbook/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'evt^t4rl=lu@b#2(p%8#myvm2trd_#zw6dsocf3kh46%6b2xg4'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
